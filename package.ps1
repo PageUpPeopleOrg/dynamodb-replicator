fcd1d39d726d58e3b3f13c6d3a62e69643fd4cf6
denisv <denisv@pageuppeople.com>
2016-08-03 13:52:17 +1000

    improvements to powershell script

--- package.ps1
+++ package.ps1
@@ -1,26 +1,35 @@
-Write-Output "Packaging Lambda app"
-Remove-Item -Recurse -Force .\package
-New-Item .\package -type directory -f
-New-Item .\package\temp -type directory -f
+﻿Write-Output "Packaging Lambda app"
+if (Test-Path .\package) {
+  Remove-Item .\package -Recurse -Force
+}
+
+
+New-Item .\package -type directory -f | Out-Null
+New-Item .\package\temp -type directory -f | Out-Null
+Write-Output "Copying dependencies..."
 Copy-Item .\backup.js .\package\temp\
 Copy-Item .\diff.js .\package\temp\
 Copy-Item .\s3-backfill.js .\package\temp\
 Copy-Item .\s3-snapshot.js .\package\temp\
-Copy-Item .\node_modules\ .\package\temp\ -recurse
+robocopy  .\node_modules\ .\package\temp\ /E | Out-Null
+Write-Output "Dependencies sorted"
 
-$environmentFile = Read-Host -Prompt 'Enter the name of the config file you would like to add to your deployment (e.g. dcx.env)'
+$environmentFile = Read-Host -Prompt 'Enter the name of the config file you would like to add to your deployment (e.g. Forms-dc4.env)'
 
-$currentdir = $PSScriptRoot
-
-New-Item .\package\temp\index.js -type file -f
-
+Write-Output "Generating output..."
+New-Item .\package\temp\index.js -type file -f | Out-Null
 get-content .\config\$environmentFile,".\index.js" | set-content ".\package\temp\index.js"
-
-$packageName = "LambdaFunction"
-
-$destination = $currentdir + "\package\" + $packageName + ".zip"
+Write-Output "Output generated"
 
 Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::CreateFromDirectory($currentdir + "\package\temp", $destination)
+$currentPath = (Get-Item -Path ".\" -Verbose).FullName
+$sourcePath = $currentPath + "\package\temp"
+$outputFile = $currentPath + "\LambdaFunction.zip"
 
-Write-Output "lambda app ready to be uploaded. Located at: " $destination
+if (Test-Path $outputFile) {
+  Remove-Item $outputFile -Force
+}
+
+[io.compression.zipfile]::CreateFromDirectory($sourcePath, $outputFile)
+
+Write-Output "λ function ready to be uploaded at: $($outputFile)"
